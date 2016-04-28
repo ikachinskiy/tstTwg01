dd3c32550728d40268240920f15a439a1a3624ed
smartnet <ivan@kachinskiy.ru>
2016-04-28 17:50:19 +0300

    Перенесли проект на площадку "ИСА"

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <title>Тест Twig 01-1</title>
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
-</head>
-<body>
-    <?php
-    require_once 'vendor/autoload.php';
-    Twig_Autoloader::register();
-    $loader = new Twig_Loader_Filesystem('template');
-    $twig = new Twig_Environment($loader);
+<?php
+/**
+ * Created by PhpStorm.
+ * User: smartnet
+ * Date: 28.04.2016
+ * Time: 17:24
+ */
+require_once 'vendor/twig/twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('template');
+$twig = new Twig_Environment($loader);
 
-    echo $twig->render('1.html.twig', array('name' => 'Начальник'));
-    ?>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
+echo $twig->render('main.html.twig', array(
+    'title'     => 'Тест Twig: 01-2',
+    'author'    => 'Иван Качинский'
+    ));
+?>
